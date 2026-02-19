fd5d577d95a0823a891c2dd6a0dd82ddae6c042c
strongo01 <maxveldhuis@outlook.com>
2026-02-19 16:05:14 +0100

    Add initial comments to homescreen and create foodscreen with relevant notes

---
+++ filmflix/lib/views/foodscreen.dart
@@ -0,0 +1,4 @@
+//STEVEN
+
+//thuisbezorgd: https://www.thuisbezorgd.nl/bestellen/eten/leidsche-rijn-3543?q=hotdog
+// q={XXXXX} die XXXXX is de zoekterm, in dit geval hotdog. Je kan hier ook meerdere woorden gebruiken, bijvoorbeeld: hotdog met friet, dan wordt het q=hotdog+met+friet.
--- filmflix/lib/views/homescreen.dart
+++ filmflix/lib/views/homescreen.dart
@@ -1,3 +1,4 @@
+//BURAK
 import 'package:flutter/material.dart';
 
 void main() {
