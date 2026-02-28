0fbc26885d1815ef7943cfef3e230b26863d45a8
strongo01 <maxveldhuis@outlook.com>
2026-02-28 16:40:05 +0100

    Update versionCode to 2 for Play Store uploads

--- filmflix/android/app/build.gradle.kts
+++ filmflix/android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        versionCode = 2 //elke upload naar google play store + 1
         versionName = flutter.versionName
     }
 
