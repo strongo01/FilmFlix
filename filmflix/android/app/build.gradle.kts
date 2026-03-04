9eef80f58fb70a39798ee3e8b4d8bd9d9a8c833b
strongo01 <maxveldhuis@outlook.com>
2026-03-04 21:23:31 +0100

    Bump versionCode to 8 for Play Store upload

--- filmflix/android/app/build.gradle.kts
+++ filmflix/android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 7 //elke upload naar google play store + 1
+        versionCode = 8 //elke upload naar google play store + 1
         versionName = flutter.versionName
     }
 
